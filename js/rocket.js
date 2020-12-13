57859ed7c2a725410b373ecdeaf00e30a82a3092
exit <exit@example.com>
2020-12-13 22:37:50 +0100

    Update

--- js/rocket.js
+++ js/rocket.js
@@ -11,6 +11,18 @@ var changeState = function(state) {
         timer = setInterval(function() {
           countdownNumber = countdownNumber -1;
           document.getElementById("countdown").innerHTML = countdownNumber;
+
+          if(countdownNumber > 4 && countdownNumber <= 7) {
+            document.getElementById("yaay").className="yaay show";
+          } else {
+            document.getElementById("yaay").className="yaay";
+          }
+          if(countdownNumber > 1 && countdownNumber <= 4) {
+            document.getElementById("cant-wait").className="cant-wait show";
+          }else {
+            document.getElementById("cant-wait").className="cant-wait";
+          }
+
           if (countdownNumber <= 0) {
              changeState(3);
           };
@@ -22,7 +34,7 @@ var changeState = function(state) {
 
           console.log("randomNumber:" , randomNumber)
 
-          if(randomNumber > 5) {
+          if(randomNumber > 9) {
             changeState(4);
           } else {
             changeState(5);
